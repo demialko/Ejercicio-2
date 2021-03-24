d16d6c1a339bf073e5bd3ffea8c8f768503ee768
demialko <demialko@gmail.com>
2021-03-24 11:22:43 -0300

    Resolucio ejercicio 2

---
+++ Ejercicio-2/Ejercicio-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de texto
+            Console.WriteLine("Ingrese un texto");
+
+            //Lectura de texto
+            string texto = Console.ReadLine();
+
+            //Impresión de texto
+            Console.WriteLine("El texto ingresado fue: " + texto);
+
+            Console.ReadKey();
+        }
+    }
+}
